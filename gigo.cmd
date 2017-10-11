c8ab21e8ee8c33da7016ff2869865797f206ce8b
Matsuyanagi <ef58ef64@gmail.com>
2017-10-12 00:29:02 +0900

    First commit.

---
+++ gigo.cmd
@@ -0,0 +1,5 @@
+@echo off
+rem 
+rem download .gitignore for go
+rem 
+wget https://raw.githubusercontent.com/github/gitignore/master/Go.gitignore -O ".gitignore"
---
+++ givs.cmd
@@ -0,0 +1,5 @@
+@echo off
+rem 
+rem download .gitignore for VisualStudio .NET
+rem 
+wget https://raw.githubusercontent.com/github/gitignore/master/VisualStudio.gitignore -O ".gitignore"
